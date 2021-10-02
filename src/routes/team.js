26e3e6332db78d46ba9c7dcf385896edf0e9e194
Nehal Gala <nehalgala@Apples-MacBook-Pro.local>
2021-10-02 19:22:41 +0530

    final commit

--- src/routes/team.js
+++ src/routes/team.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const TeamController = require('../controllers/team.controller');
 
-// Create routes for product here
+// Create routes for team here
 module.exports = router;
